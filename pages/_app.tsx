2ae1ea9a85347cdb1b180fb10f470e2fb415634f
harry <harry96sn@gmail.com>
2025-01-13 11:09:21 -0500

    correct types errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-const MyApp = ({ Component, pageProps, router }: { Component: any; pageProps: any; router: any }) => {
+const MyApp = ({ Component, pageProps, router }: AppProps) => {
   // Lista de rutas sin Layout (como Login)
-  const noLayoutRoutes = ["/login"];
+  const noLayoutRoutes: string[] = ["/login"];
 
   // Determinar si la página actual necesita el Layout
   const isNoLayoutRoute = noLayoutRoutes.includes(router.pathname);
